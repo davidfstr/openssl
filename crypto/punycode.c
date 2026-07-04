8a6853fb8e3205d5c53ecb68d44fdba5f498cba0
David Foster <david@dafoster.net>
2026-07-04 14:03:51 -0400

    ossl_a2ulabel: Partially prove assert far_top via new loop invariant and trailing loop assert next_len

--- crypto/punycode.c
+++ crypto/punycode.c
@@ -375,7 +375,9 @@ int ossl_a2ulabel(const char *in, char *out, size_t outlen)
     if (!WPACKET_init_static_len(&pkt, (unsigned char *)out, outlen, 0))
         return -1;
 
-    /*@ loop invariant inptr_str: valid_read_string(inptr); */
+    /*@ loop invariant inptr_str: valid_read_string(inptr);
+        loop invariant inptr_len: strlen(inptr) <= PTRDIFF_MAX;
+    */
     while (1) {
         const char *tmpptr = strchr(inptr, '.');
         size_t delta = tmpptr != NULL ? (size_t)(tmpptr - inptr) : strlen(inptr);
@@ -430,6 +432,7 @@ int ossl_a2ulabel(const char *in, char *out, size_t outlen)
             result = 0;
 
         /*@ assert next_str: valid_read_string(tmpptr + 1); */
+        /*@ assert next_len: strlen(tmpptr + 1) <= PTRDIFF_MAX; */
         inptr = tmpptr + 1;
     }
 
